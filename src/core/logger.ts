183f2b887d7e16fa07f26d525a81a43e45e39c7d
Jonathan Chan <cusxio@gmail.com>
2025-12-16 06:36:49 +0000

    chore: pino-pretty in dev

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -7,4 +7,8 @@ const require = createRequire(import.meta.url)
 
 const { pino } = require('pino') as typeof import('pino')
 
-export const logger = pino()
+export const logger = pino({
+  ...(import.meta.env.DEV && {
+    transport: { target: 'pino-pretty', options: { colorize: true } },
+  }),
+})
