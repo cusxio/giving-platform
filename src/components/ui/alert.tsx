fba2f4bac33c0c06d28846f5fd4377c617f35d9c
Jonathan Chan <cusxio@gmail.com>
2026-01-03 13:13:02 +0000

    feat: info alert

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -2,13 +2,21 @@ import type { HTMLAttributes } from 'react'
 
 import { cx } from '#/styles/cx'
 
-interface AlertProps extends HTMLAttributes<HTMLDivElement> {}
+interface AlertProps extends HTMLAttributes<HTMLDivElement> {
+  variant?: 'error' | 'info'
+}
+
+const alertVariants = {
+  error: cx('bg-base-error text-fg-error'),
+  info: cx('bg-base-info text-fg-info'),
+}
 
 export function Alert(props: AlertProps) {
-  const { className, ...rest } = props
+  const { className, variant = 'error', ...rest } = props
+
   return (
     <div
-      className={cx('bg-base-error p-2 text-sm text-fg-error', className)}
+      className={cx('p-2 text-sm', alertVariants[variant], className)}
       {...rest}
     />
   )
