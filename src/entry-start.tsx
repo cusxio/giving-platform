2b52ab0202d27f4914c3e07259acf428482c4b5a
Jonathan Chan <cusxio@gmail.com>
2026-02-26 04:43:49 +0000

    chore: correctly revert nitro

--- src/entry-start.tsx
+++ src/entry-start.tsx
@@ -1,15 +1,18 @@
 import { createStart } from '@tanstack/react-start'
 
-import {
-  loggingMiddleware,
-  maintenanceMiddleware,
-  sessionMiddleware,
-} from './server/middleware'
+export const startInstance = createStart(async () => {
+  if (!import.meta.env.SSR) {
+    return {}
+  }
 
-export const startInstance = createStart(() => ({
-  requestMiddleware: [
-    maintenanceMiddleware,
-    sessionMiddleware,
-    loggingMiddleware,
-  ],
-}))
+  const { loggingMiddleware, maintenanceMiddleware, sessionMiddleware } =
+    await import('./server/middleware')
+
+  return {
+    requestMiddleware: [
+      maintenanceMiddleware,
+      sessionMiddleware,
+      loggingMiddleware,
+    ],
+  }
+})
